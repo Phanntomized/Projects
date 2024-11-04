3fb85de6aa1937ea08c19bba9157c531944021fd
Brandon Smith <88860661+smithbwlhs@users.noreply.github.com>
2024-11-03 22:54:29 -0800

    Update loop_fun.py

--- LoopFun/loop_fun.py
+++ LoopFun/loop_fun.py
@@ -2,7 +2,19 @@
 
 def main():
     pass #delete this line
-    # add your code here
+    # add your code here - suggested parts added
+
+    # Part 1 - get user input
+
+    # Part 2a - print numbers
+
+    # Part 2b - calculate and print average of the numbers
+
+
+    # Part 3 - print the hollow box
+
+
+    # Part 4 - print the triangular shape
 
 if __name__ == '__main__':
     main() # don't change this
